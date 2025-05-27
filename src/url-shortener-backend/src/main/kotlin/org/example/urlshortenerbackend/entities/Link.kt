37fb400acddaa7a8493bef813c32fd655e5c4a71
furaizi <furachannel16@gmail.com>
2025-05-27 22:08:34 +0300

    Create Link entity class

---
+++ src/url-shortener-backend/src/main/kotlin/org/example/urlshortenerbackend/entities/Link.kt
@@ -0,0 +1,44 @@
+package org.example.urlshortenerbackend.entities
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Version
+import org.hibernate.annotations.CreationTimestamp
+import java.time.LocalDateTime
+
+@Entity
+class Link(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var linkId: Long,
+
+    @Column(length = 8, nullable = false, unique = true)
+    var shortCode: String,
+
+    @Column(nullable = false)
+    var originalUrl: String,
+
+    var clicks: Long = 0L,
+
+    var uniqueVisitors: Long = 0L,
+
+    var lastAccessedAt: LocalDateTime? = null,
+
+    var expiresAt: LocalDateTime? = null,
+
+    // Soft delete flag
+    var isActive: Boolean = true,
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    var createdAt: LocalDateTime? = null,
+    // nullable because it will be set automatically on creation by Hibernate
+    var updatedAt: LocalDateTime? = null,
+
+    // Optimistic lock for concurrent updates of "clicks"
+    @Version
+    var version: Long? = null
+)
