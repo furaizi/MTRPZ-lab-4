024dee631fb33f3ca890775089c2f20ffa64e060
Mykhailo Skrypnyk <57300740+elaiviaien@users.noreply.github.com>
2025-05-28 19:22:02 +0300

    infra: terraform config iam roles  (#4)
    * infra: add lambda role
    
    * infra: policy for Lambda to access DynamoDB and write logs
    
    * infr: add basic terraform files
    
    * infra: fix terraform config

---
+++ infra/iam/policies.tf
@@ -0,0 +1,34 @@
+data "aws_iam_policy_document" "lambda_policy" {
+  statement {
+    actions = [
+      "dynamodb:PutItem",
+      "dynamodb:GetItem",
+      "dynamodb:UpdateItem",
+      "dynamodb:Query"
+    ]
+    resources = [
+      "arn:aws:dynamodb:${var.aws_region}:${var.aws_account_id}:table/ShortLinks",
+      "arn:aws:dynamodb:${var.aws_region}:${var.aws_account_id}:table/Analytics"
+    ]
+  }
+
+  statement {
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents"
+    ]
+    resources = ["arn:aws:logs:*:*:*"]
+  }
+}
+
+resource "aws_iam_policy" "lambda_exec_policy" {
+  name        = "lambda-exec-policy"
+  description = "IAM policy for Lambda to access DynamoDB and write logs"
+  policy      = data.aws_iam_policy_document.lambda_policy.json
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_policy_attach" {
+  role       = aws_iam_role.lambda_exec_role.name
+  policy_arn = aws_iam_policy.lambda_exec_policy.arn
+}
