2dd5899676e5b71ffb39db6e3ff97c940d39284f
elaiviaien <skrypnyk.michael@gmail.com>
2025-05-30 17:48:03 +0300

    infra: rm analytics table

--- infra/main.tf
+++ infra/main.tf
@@ -15,5 +15,4 @@ module "iam" {
   aws_region         = var.aws_region
   aws_account_id      = data.aws_caller_identity.current.account_id
   shortlinks_table_arn = module.dynamodb.shortlinks_table_arn
-  analytics_table_arn  = module.dynamodb.analytics_table_arn
 }
