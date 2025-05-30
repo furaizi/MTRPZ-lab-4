c58cd7e1ba933fafef1746af421fd9db8fde0018
elaiviaien <skrypnyk.michael@gmail.com>
2025-05-30 16:52:41 +0300

    infra: dynamodb config and policies for lambda

--- infra/main.tf
+++ infra/main.tf
@@ -4,8 +4,16 @@ provider "aws" {
 
 data "aws_caller_identity" "current" {}
 
-module "iam" {
-  source        = "./iam"
-  aws_region    = var.aws_region
+module "dynamodb" {
+  aws_region = var.aws_region
   aws_account_id = data.aws_caller_identity.current.account_id
+  source = "./dynamodb"
+}
+
+module "iam" {
+  source              = "./iam"
+  aws_region         = var.aws_region
+  aws_account_id      = data.aws_caller_identity.current.account_id
+  shortlinks_table_arn = module.dynamodb.shortlinks_table_arn
+  analytics_table_arn  = module.dynamodb.analytics_table_arn
 }
